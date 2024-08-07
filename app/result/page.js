b636ffd362ad6cb81cc95aa4409e373393ba7d54
TinghuanHsu <tinghuanhsu@uitctech.com.tw>
2024-08-07 21:12:24 +0800

    feat: change result block getting data method

--- app/result/page.js
+++ app/result/page.js
@@ -1,17 +1,54 @@
-import { Client } from "@notionhq/client";
-import RenderResultBlock from "./renderResultBlock";
+"use client";
 
-export default async function Result() {
-  const notion = new Client({ auth: process.env.NOTION_TOKEN });
-  const pages = await notion.databases.query({
-    database_id: process.env.NOTION_DATABASE_ID,
-  });
+//import RenderResultBlock from "./renderResultBlock";
+import ResultBlock from "../components/ResultBlock";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+export default function Result() {
+  const [pages, setPages] = useState();
+
+  useEffect(() => {
+    getPages();
+  }, []);
+
+  const getPages = async () => {
+    try {
+      const response = await axios.get(`/api`);
+
+      console.log("response ===> ", response.data);
+
+      setPages(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <>
-      <div className="grid md:grid-cols-8 grid-cols-2">
-        <RenderResultBlock pages={pages}></RenderResultBlock>
-      </div>
+      {pages && (
+        <div className="grid md:grid-cols-8 grid-cols-2">
+          <ResultBlock data={pages.results[0]} title={"急救箱"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"算盤"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"相機"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"金幣"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"鎚子"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"樂器"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"鍵盤"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"飛機"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"書"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"麥克風"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"調色盤"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"廚師帽"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"手槍"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"板手"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"博士帽"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"場記板"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"黑板"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"三角尺"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"特斯拉"}></ResultBlock>
+        </div>
+      )}
     </>
   );
 }
