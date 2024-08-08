26babc637facff828bd70c3ef577d2b70c0f3d8f
TinghuanHsu <tinghuanhsu@uitctech.com.tw>
2024-08-08 12:05:37 +0800

    feat: sync data every 5 secs in Result page

--- app/result/page.js
+++ app/result/page.js
@@ -1,6 +1,5 @@
 "use client";
 
-import RenderResultBlock from "./renderResultBlock";
 import ResultBlock from "../components/ResultBlock";
 import axios from "axios";
 import { useState, useEffect } from "react";
@@ -10,13 +9,19 @@ export default function Result() {
 
   useEffect(() => {
     getPages();
+
+    const intervalId = setInterval(() => {
+      getPages();
+    }, 5 * 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
   }, []);
 
   const getPages = async () => {
     try {
       const response = await axios.get(`/api`);
-
-      console.log("response ===> ", response.data);
 
       setPages(response.data);
     } catch (error) {
@@ -28,7 +33,29 @@ export default function Result() {
     <>
       {pages && (
         <div className="grid md:grid-cols-10 grid-cols-2">
-          <RenderResultBlock pages={pages}></RenderResultBlock>
+          <ResultBlock data={pages.results[0]} title={"急救箱"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"算盤"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"相機"}></ResultBlock>
+          <ResultBlock
+            data={pages.results[0]}
+            title={"十萬元大紅包"}
+          ></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"鎚子"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"樂器"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"鍵盤"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"飛機"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"書"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"麥克風"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"調色盤"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"廚師帽"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"手槍"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"板手"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"博士帽"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"場記板"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"黑板"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"三角尺"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"特斯拉"}></ResultBlock>
+          <ResultBlock data={pages.results[0]} title={"未知"}></ResultBlock>
         </div>
       )}
     </>
