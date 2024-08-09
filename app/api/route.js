81d96c292e40b36cb97ef70441df202022ae174a
HsuTingHuan <s780609@gmail.com>
2024-08-09 11:52:42 +0800

    feat: add delete button to selector

--- app/api/route.js
+++ app/api/route.js
@@ -13,6 +13,12 @@ export async function GET(request) {
 export async function PATCH(request) {
   const requestBody = await request.json();
 
+  // 刪除資料
+  if (requestBody.operation?.toUpperCase() === "DELETE") {
+    console.log("requestBody.operation ===> ", requestBody.operation);
+    return deleteData(requestBody);
+  }
+
   if (!requestBody) {
     return NextResponse.json({ message: "request is null" }, { status: 400 });
   }
@@ -45,6 +51,53 @@ export async function PATCH(request) {
             type: "text",
             text: {
               content: plainText,
+            },
+          },
+        ],
+      },
+    },
+  });
+
+  return NextResponse.json({ result: page }, { status: 200 });
+}
+
+async function deleteData(requestBody) {
+  if (!requestBody) {
+    return NextResponse.json({ message: "request is null" }, { status: 400 });
+  }
+
+  if (!requestBody.pageId) {
+    return NextResponse.json({ message: "pageId is null" }, { status: 400 });
+  }
+
+  const notion = new Client({ auth: process.env.NOTION_TOKEN });
+  const pages = await notion.databases.query({
+    database_id: process.env.NOTION_DATABASE_ID,
+  });
+
+  let plainText =
+    pages.results[0].properties[requestBody.propName].rich_text[0]?.plain_text;
+
+  if (!plainText) {
+    return NextResponse.json({ message: "plainText is null" }, { status: 400 });
+  }
+
+  const textArray = plainText.split(",");
+
+  textArray.splice(textArray.indexOf(requestBody.text), 1);
+
+  const newTextArray = textArray.join(",");
+
+  const page = await notion.pages.update({
+    page_id: requestBody.pageId,
+    properties: {
+      [requestBody.propName]: {
+        type: "rich_text",
+        rich_text: [
+          {
+            type: "text",
+            text: {
+              content: newTextArray,
             },
           },
         ],
--- app/components/Selector.js
+++ app/components/Selector.js
@@ -51,6 +51,40 @@ export function Selector({
     }
   };
 
+  const deleteData = async () => {
+    if (!pageData) {
+      return;
+    }
+
+    try {
+      setLoading(true);
+      setCount(count - 1);
+      setVote(vote - 1);
+
+      const response = await axios.patch(
+        `/api`,
+        {
+          pageId: pageData.id,
+          propName: title,
+          text: myName,
+          operation: "delete",
+        },
+        {
+          headers: {},
+        }
+      );
+
+      setPageData(response.data.result);
+    } catch (error) {
+      setCount(count - 1);
+      setVote(vote - 1);
+
+      throw error;
+    } finally {
+      setLoading(false);
+    }
+  };
+
   let buttonStyle = "bg-blue-500 text-white rounded m-1 px-3 py-1";
 
   if (disabled) {
@@ -62,58 +96,87 @@ export function Selector({
       {" "}
       <Toaster></Toaster>
       <div className="w-full px-2 py-2 my-5">
-        <div className="text-center text-xl">
-          <button
-            onClick={async () => {
-              if (!myName) {
-                alert("請輸入你的名字");
+        <div className="flex flex-col text-center text-xl">
+          <div>
+            <button
+              onClick={async () => {
+                if (!myName) {
+                  alert("請輸入你的名字");
 
-                return;
-              }
+                  return;
+                }
 
-              if (count >= 3) {
-                toast.error("不能投票超過三次");
+                if (count >= 3) {
+                  toast.error("不能投票超過三次");
 
-                return;
-              }
+                  return;
+                }
 
-              plus();
-            }}
-            disabled={loading || disabled}
-            className={buttonStyle}
-          >
-            {loading && (
-              <svg
-                className="animate-spin h-5 w-5 mr-3 text-white"
-                viewBox="0 0 24 24"
-              >
-                <circle
-                  className="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                ></circle>
-                <path
-                  className="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                ></path>
-              </svg>
-            )}
-            {loading || "選這個"}
-          </button>
-          <span>{vote}</span>
-          {/* <button
-          onClick={() => {
-            alert("不能減少");
-            //setCount(count - 1);
-          }}
-          className={buttonStyle}
-        >
-          -
-        </button> */}
+                plus();
+              }}
+              disabled={loading || disabled}
+              className={buttonStyle}
+            >
+              {loading && (
+                <svg
+                  className="animate-spin h-5 w-5 mr-3 text-white"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+              )}
+              {loading || "選這個"}
+            </button>
+          </div>
+          <div>投了{vote}票</div>
+          <div>
+            <button
+              onClick={() => {
+                if (count === 0) {
+                  toast.error("你都還沒投票");
+
+                  return;
+                }
+
+                deleteData();
+              }}
+              className={buttonStyle}
+            >
+              {loading && (
+                <svg
+                  className="animate-spin h-5 w-5 mr-3 text-white"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+              )}
+              {loading || "後悔選這個"}
+            </button>
+          </div>
         </div>
         <div className="text-center text-xl">{title}</div>
         <ImageLoader src={src}></ImageLoader>
